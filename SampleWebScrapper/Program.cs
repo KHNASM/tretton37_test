d8af83d6378b4bcc2ffed9d0fb5202c9e6841b65
KHNASM <au.asre@gmail.com>
2024-06-08 21:25:10 -0700

    Project files added Implementation of the POC Web Scrapper - WIP

---
+++ SampleWebScrapper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+
+namespace SampleWebScrapper;
+
+public class Program
+{
+
+    public static async Task Main(string[] args)
+    {
+        const string baseUrl = "https://books.toscrape.com/";
+        const string outputDirectory = "E:\\_Scrapper_Output";
+
+        if(Directory.Exists(outputDirectory))
+        {
+            Directory.Delete(outputDirectory, true);
+        }
+
+        Directory.CreateDirectory(outputDirectory);
+
+        WebScrapper scrapper = new(baseUrl, outputDirectory);
+
+        await scrapper.RunScraperAsync();
+    }
+
+}
---
+++ SampleWebScrapper/WebScrapper.cs
@@ -0,0 +1,165 @@
+﻿using HtmlAgilityPack;
+
+namespace SampleWebScrapper;
+
+internal class WebScrapper
+{
+    private readonly Queue<string> _queue = new();
+    private readonly HashSet<string> _completed = new();
+
+    private readonly Uri _baseUri;
+    private readonly string _startingUrl;
+
+    private readonly string _outputRootDirectory;
+
+
+    public WebScrapper(string url, string outputRootDirectory)
+    {
+        if (string.IsNullOrWhiteSpace(url))
+        {
+            throw new ArgumentException($"'{nameof(url)}' cannot be null or whitespace.", nameof(url));
+        }
+
+        _outputRootDirectory = string.IsNullOrWhiteSpace(outputRootDirectory)
+            ? "_SampleWebscrapperOutput"
+            : outputRootDirectory;
+
+
+        if (!Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out Uri? uri) || uri == null || !uri.IsAbsoluteUri)
+        {
+            throw new ArgumentException($"'{nameof(url)}' must be a valid absolute url.", nameof(url));
+        }
+
+        string baseUrl = uri.GetLeftPart(UriPartial.Authority);
+
+        _baseUri = new Uri(baseUrl);
+        _startingUrl = uri.GetLeftPart(UriPartial.Path).TrimEnd('/', '\\');
+    }
+
+    public async Task RunScraperAsync()
+    {
+        _queue.Enqueue(_startingUrl);
+
+        while (_queue.TryDequeue(out var url))
+        {
+            await DownloadPage(url);
+        }
+    }
+
+    private async Task DownloadPage(string url)
+    {
+        if (!Uri.TryCreate(url, UriKind.Absolute, out Uri? uri) || uri == null)
+        {
+            await WriteLineAsync($"X X X X X X Unable to download page becase '{url}' is not a valid absolute url"); // TODO: revisit to improve this
+            return;
+        }
+
+        if (uri.Host != _baseUri.Host)
+        {
+            await WriteLineAsync($"X X X X X X Unable to download page becase '{url}' is not on the same domain as the base url"); // TODO: revisit to improve this
+            return;
+        }
+
+        string localPath = ComputePath(_outputRootDirectory, uri);
+        if (localPath == _outputRootDirectory) // TODO: revisit to improve this
+        {
+            localPath = Path.Combine(localPath, "index.html");
+        }
+        if (File.Exists(localPath))
+        {
+            await WriteLineAsync($"I I I I Page already downloaded: {url}"); // TODO: revisit to improve this
+            return;
+        }
+
+        using var client = new HttpClient();
+        var response = await client.GetAsync(uri);
+
+        HttpContent content = response.Content;
+
+        if (!response.IsSuccessStatusCode)
+        {
+            Console.WriteLine($"X X X X X X Failed to download {url}: Server returned: HTTP {(int)response.StatusCode}"); // TODO: revisit to improve this
+            return;
+        }
+
+        using Stream stream = await response.Content.ReadAsStreamAsync();
+        await SaveStreamAsFileAsync(stream, localPath);
+
+        Console.WriteLine($":) :) :) Downloaded {url}"); // TODO: revisit to improve this
+
+        QueueLinksFromFileAsync(localPath, uri);
+    }
+
+    private void QueueLinksFromFileAsync(string path, Uri currentUri)
+    {
+        HtmlDocument doc = new();
+
+        doc.Load(path); // TODO: Async? non-html?
+
+        var links = doc.DocumentNode.Descendants("a")
+            .Select(a => a.GetAttributeValue("href", null))
+            .Where(href => !string.IsNullOrWhiteSpace(href))
+            .ToArray();
+
+        foreach (string link in links)
+        {
+            if (!Uri.TryCreate(link, UriKind.RelativeOrAbsolute, out Uri? uri) || uri == null)
+            {
+                continue;
+            }
+            
+            if (!uri.IsAbsoluteUri)
+            {
+                uri = new Uri(currentUri, uri);
+            }
+
+            if (uri.IsAbsoluteUri && uri.Host != _baseUri.Host)
+            {
+                continue;
+            }
+
+            string absoluteUrl = uri.GetLeftPart(UriPartial.Path).TrimEnd('/', '\\');
+
+            _queue.Enqueue(absoluteUrl);
+        }
+    }
+
+    private static string ComputePath(string rootDirectory, Uri uri)
+    {
+        if (string.IsNullOrWhiteSpace(rootDirectory))
+        {
+            throw new ArgumentException($"'{nameof(rootDirectory)}' cannot be null or whitespace.", nameof(rootDirectory));
+        }
+
+        ArgumentNullException.ThrowIfNull(uri);
+
+        if (!uri.IsAbsoluteUri)
+        {
+            throw new ArgumentException($"'{nameof(uri)}' must be an absolute uri", nameof(uri));
+        }
+
+        string localPath = uri.LocalPath
+            .Trim('/', '\\')
+            .Replace('/', '\\');
+
+        localPath = Path.Combine(rootDirectory, localPath);
+
+        return localPath;
+    }
+
+    private static async Task SaveStreamAsFileAsync(Stream stream, string localPath)
+    {
+        string destinationDirectory = Path.GetDirectoryName(localPath)!;
+
+        if (!Directory.Exists(destinationDirectory))
+        {
+            Directory.CreateDirectory(destinationDirectory);
+        }
+
+        using var fileStream = File.Create(localPath);
+        await stream.CopyToAsync(fileStream);
+    }
+
+
+    private Task WriteLineAsync(string text) => Console.Out.WriteLineAsync(text);
+}
